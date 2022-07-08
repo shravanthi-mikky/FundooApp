7a49fc49afbb1abff0810ea2a14ebb8a02c1e563
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-07-08 13:01:49 +0530

    [shravanthi] - Created usertable in database using Code First Apprach, created Register API (POST)

---
+++ BusinessLayer/Interfaces/IUserBL.cs
@@ -0,0 +1,13 @@
+﻿using CommonLayer.Model;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IUserBL
+    {
+        public UserEntity Register(UserRegistrationModel userRegistrationModel);
+    }
+}
---
+++ FundooApplication/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Interfaces;
+using CommonLayer.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FundooApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        IUserBL iUserBL;
+        public UserController(IUserBL iUserBL)
+        {
+            this.iUserBL = iUserBL;
+        }
+        [HttpPost("Register")]
+        public IActionResult Register(UserRegistrationModel userRegistrationModel)
+        {
+            try
+            {
+                var result= iUserBL.Register(userRegistrationModel);
+                if (result != null)
+                {
+                    return this.Ok(new { Success = true, message = "Registration Successfull", Data = result });
+                }
+                else
+                {
+                    return this.BadRequest(new { Success = false, message = "Registration Unsuccessfull"});
+                }
+            }
+            catch(Exception e)
+            {
+                throw;
+            }
+        }
+
+    }
+}
